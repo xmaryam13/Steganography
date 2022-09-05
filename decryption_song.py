c6efd86f197356701d9d584f2929b29230c533a4
xmaryam13 <90093129+xmaryam13@users.noreply.github.com>
2022-09-05 22:10:56 +0100

    Add files via upload

---
+++ decryption_song.py
@@ -0,0 +1,13 @@
+import wave
+song = wave.open("C:/Users/MBajw/Downloads/Nursery_songs.mp3", mode='rb')
+
+frame_bytes = bytearray(list(song.readframes(song.getnframes())))
+
+
+extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
+string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
+decoded = string.split("###")[0]
+
+
+print("Sucessfully decoded: "+decoded)
+song.close()
